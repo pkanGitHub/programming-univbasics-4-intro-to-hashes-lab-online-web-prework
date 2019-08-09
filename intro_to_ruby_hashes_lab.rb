46afbc49620d0ade26df44b2c33ddfe055894f0c
Phyllis Kan <pyk9q7@gmail.com>
2019-08-09 13:10:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,11 +1,19 @@
 def new_hash
   # return an empty hash
+<<<<<<< HEAD
   hash = Hash.new
+=======
+  new = Hash.new
+>>>>>>> 38f3a98e3235f967b5addf80467c45df92e303d7
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+<<<<<<< HEAD
   hash = {
+=======
+  new = {
+>>>>>>> 38f3a98e3235f967b5addf80467c45df92e303d7
     "Surah" => "Restaurant",
     "Jin" => "Manager"
   }
@@ -13,18 +21,30 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+<<<<<<< HEAD
   hash = {:name => "Grace Hopper"}
+=======
+  new = {:name => "Grace Hopper"}
+>>>>>>> 38f3a98e3235f967b5addf80467c45df92e303d7
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+<<<<<<< HEAD
   hash = {:id => 6}
+=======
+  new = {:id => 6}
+>>>>>>> 38f3a98e3235f967b5addf80467c45df92e303d7
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   new = {
+<<<<<<< HEAD
   key => value
+=======
+  :name => "#{key}"
+>>>>>>> 38f3a98e3235f967b5addf80467c45df92e303d7
   }
 end
 
